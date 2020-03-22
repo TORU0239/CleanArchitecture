66d9b293035307884d07f9ddbd8e6eb36b13a81f
Toru Choi <blueocean239@gmail.com>
2020-03-22 20:47:28 +0800

    newly initialize activity

---
+++ app/src/main/java/sg/toru/cleanarchitecture/presentation/ui/activity/InitActivity.kt
@@ -0,0 +1,13 @@
+package sg.toru.cleanarchitecture.presentation.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import sg.toru.cleanarchitecture.R
+
+class InitActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_init)
+    }
+}
