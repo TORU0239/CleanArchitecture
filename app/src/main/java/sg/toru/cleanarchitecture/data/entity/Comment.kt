1ff68773c79ebf4f14a3afa3163a28fc02d2fdfe
Toru Choi <blueocean239@gmail.com>
2020-03-22 20:11:09 +0800

    deleted all kotlin code, to start new one fromt the scratch

--- app/src/main/java/sg/toru/cleanarchitecture/data/entity/Comment.kt
+++
@@ -1,9 +0,0 @@
-package sg.toru.cleanarchitecture.data.entity
-
-data class Comment (
-    val postId:String,
-    val id:String,
-    val name:String,
-    val email:String,
-    val body:String
-)
--- app/src/main/java/sg/toru/cleanarchitecture/data/util/NetworkUtil.kt
+++
@@ -1,8 +0,0 @@
-package sg.toru.cleanarchitecture.data.util
-
-object NetworkUtil {
-    const val endpoint = "https://jsonplaceholder.typicode.com"
-    const val post = "/posts"
-    const val comment = "/comments"
-    const val postQuery = "postId"
-}
